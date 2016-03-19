18d9c1a833882938dc9da5ecb6b5ede44af2738f
nashilnik <bogdan.nashilnik@gmail.com>
2016-03-19 14:12:08 +0200

    Версия 1.0.

---
+++ FightClubLogic/Battle.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace FightClubLogic
+{
+    public class Battle
+    {
+        private Fighter fighter1;
+        private Fighter fighter2;
+        private int round = 1;
+        private RoundHalf roundHalf = RoundHalf.Attack;
+        private Random rng = new Random();
+        public delegate void RoundChange(Battle sender);
+        public event RoundChange RoundChanged;
+        public event RoundChange RoundHalfChanged;
+
+        public Fighter Fighter1
+        {
+            get
+            {
+                return fighter1;
+            }
+        }
+        public Fighter Fighter2
+        {
+            get
+            {
+                return fighter2;
+            }
+        }
+        public int Round
+        {
+            get
+            {
+                return round;
+            }
+        }
+        public RoundHalf RoundHalf
+        {
+            get
+            {
+                return roundHalf;
+            }
+        }
+
+        public Battle(Fighter fighter1, Fighter fighter2)
+        {
+            this.fighter1 = fighter1;
+            this.fighter2 = fighter2;
+        }
+
+        public void Action(BodyPart bodyPartAttack, BodyPart bodyPartDefend)
+        {
+            this.fighter2.SetBlock(bodyPartDefend);
+            this.fighter2.GetHit(bodyPartAttack, fighter1.Damage);
+            Fighter temp = this.fighter1;
+            this.fighter1 = this.fighter2;
+            this.fighter2 = temp;
+            if (this.roundHalf == RoundHalf.Attack)
+            {
+                this.roundHalf = RoundHalf.Defend;
+                this.RoundHalfChanged(this);
+            }
+            else
+            {
+                this.roundHalf = RoundHalf.Attack;
+                this.RoundHalfChanged(this);
+                this.round++;
+                this.RoundChanged(this);
+            }
+        }
+        public void AttackCPU(BodyPart bodyPartAttack)
+        {
+            this.Action(bodyPartAttack, GenerateBodyPart());
+        }
+        public void DefendFromCPU(BodyPart bodyPartDefend)
+        {
+            this.Action(GenerateBodyPart(), bodyPartDefend);
+        }
+        private BodyPart GenerateBodyPart()
+        {
+            int totalBodyParts = Enum.GetValues(typeof(BodyPart)).Length;
+            return (BodyPart)rng.Next(0, totalBodyParts);
+        }
+    }
+}
---
+++ FightClubLogic/Fighter.cs
@@ -0,0 +1,94 @@
+﻿namespace FightClubLogic
+{
+    public class Fighter
+    {
+        private string name;
+        private int maxHP;
+        private int hp;
+        private int damage;
+        private BodyPart blocked;
+        private string imagePath = null;
+        public delegate void HitEvent(Fighter sender);
+        public delegate void HitEventWithDamage(Fighter sender, int damage);
+        public event HitEvent Block;
+        public event HitEventWithDamage Wound;
+        public event HitEvent Death;
+
+        public BodyPart Blocked
+        {
+            get
+            {
+                return blocked;
+            }
+        }
+        public int Damage
+        {
+            get
+            {
+                return damage;
+            }
+        }
+        public int MaxHP
+        {
+            get
+            {
+                return maxHP;
+            }
+        }
+        public int HP
+        {
+            get
+            {
+                return hp;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public string ImagePath
+        {
+            get
+            {
+                return imagePath;
+            }
+        }
+
+        public Fighter(string name, int maxHP, int damage)
+        {
+            this.name = name;
+            this.maxHP = maxHP;
+            this.hp = maxHP;
+            this.damage = damage;
+            this.SetBlock(BodyPart.Head);
+        }
+        public Fighter(string name, int maxHP, int damage, string imagePath) : this(name, maxHP, damage)
+        {
+            this.imagePath = imagePath;
+        }
+
+        public void GetHit(BodyPart bodyPart, int damage)
+        {
+            if (bodyPart == this.blocked)
+            {
+                this.Block(this);
+            }
+            else
+            {
+                this.hp -= damage;
+                this.Wound(this, damage);
+            }
+            if (this.hp <= 0)
+            {
+                this.Death(this);
+            }
+        }
+        public void SetBlock(BodyPart bodyPart)
+        {
+            this.blocked = bodyPart;
+        }
+    }
+}
